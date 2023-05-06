c2e052148b55cc0336fc077d93ffd3c9c6dfd8b8
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2023-05-06 16:44:05 -0400

    moved files around
    had to clean up the folder

--- widgets/question.py
+++ widgets/question.py
@@ -1,8 +1,8 @@
 from PySide6.QtCore import Qt 
 from PySide6.QtWidgets import QWidget
-from ui_questionWidget import Ui_questionsWidget
+from questionsWidget.ui_questionWidget import Ui_questionsWidget
 
-class questionWidget(QWidget, Ui_questionsWidget):
+class QuestionWidget(QWidget, Ui_questionsWidget):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
